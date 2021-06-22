67963192922457b8481f03eea24bc9f73a799d59
Dip kumar saha <dip9178@gmail.com>
2021-06-22 11:52:49 +0600

    2nd validation

--- form-submission.php
+++ form-submission.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Registration Form</title>
-</head>
-<body>
 <?php
 
 $fnameErr = $lnameErr = $genderErr = $dobErr = $ReligionErr = $mailErr = $usernameErr = $passwordErr = "";
@@ -61,36 +55,39 @@ function test_input($data) {
   return $data;
 }
 ?>
-
-
-
-<h1>Registration Form</h1><br>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration Form</title>
+</head>
+<body>
+  <h1>Registration Form</h1><br>
 <fieldset>
-	<legend> Basic information: <br> </legend>
-	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  <legend> Basic information: <br> </legend>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 <label for="fname">1.First Name:<label>
-		<input type="text" id="fname" name="fname">
-		<span style="color: red;"class="error">* <?php echo $fnameErr;?></span>
-		<br>
+    <input type="text" id="fname" name="fname">
+    <span style="color: red;"class="error">* <?php echo $fnameErr;?></span>
+    <br>
 <label for="lname">2.Last Name:</label>
-		<input type="text" id="lname" name="lname">
-		<span style="color: red;"class="error">* <?php echo $lnameErr;?></span>
-		<br>
+    <input type="text" id="lname" name="lname">
+    <span style="color: red;"class="error">* <?php echo $lnameErr;?></span>
+    <br>
 <label for="Gender">3.Gender:</label>
-		<input type="radio" id="male" name="gender" value="male">
+    <input type="radio" id="male" name="gender" value="male">
 <label for="male">Male</label>
-		
-		<input type="radio" id="female" name="gender" value="female">
+    
+    <input type="radio" id="female" name="gender" value="female">
 <label for="female">Female</label>
-		
-		<input type="radio" id="other" name="gender" value="other">
+    
+    <input type="radio" id="other" name="gender" value="other">
 <label for="other">Other</label>
 <span style="color: red;"class="error">* <?php echo $genderErr;?></span>
 <br>
 <label for="dob">4.DOB:<label>
-		<input type="date" id="dob" name="dob">
-		<span style="color: red;" class="error">* <?php echo $dobErr;?></span>
-		<br>
+    <input type="date" id="dob" name="dob">
+    <span style="color: red;" class="error">* <?php echo $dobErr;?></span>
+    <br>
 
   <label for="Religion">5.Religion:</label>
   <select name="Religion" id="Religion">
@@ -99,41 +96,43 @@ function test_input($data) {
     <option value="other">Other</option>
     <span style="color: red;" class="error">* <?php echo $ReligionErr;?></span><br>
  </select>
- </form>
+ 
 </fieldset>
 <fieldset>
-	<legend>Contact information: <br></legend>
-	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-	<label for="paddress">1.Present Address:<label>
-		<input type="text" id="paddress" name="paddress"><br>
-	<label for="peraddress">2.Permanent Address:<label>
-		<input type="text" id="peraddress" name="peraddress"><br>
-	<label for="tel">3.Phone:<label>
-		<input type="tel" id="tel" name="tel"><br>
+  <legend>Contact information: <br></legend>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  <label for="paddress">1.Present Address:<label>
+    <input type="text" id="paddress" name="paddress"><br>
+  <label for="peraddress">2.Permanent Address:<label>
+    <input type="text" id="peraddress" name="peraddress"><br>
+  <label for="tel">3.Phone:<label>
+    <input type="tel" id="tel" name="tel"><br>
 
-	<label for="mail">4.Email:<label>
-		<input type="Email" id="mail" name="mail">
-		<span style="color: red;" class="error">* <?php echo $mailErr;?></span>
-		<br>		
+  <label for="mail">4.Email:<label>
+    <input type="Email" id="mail" name="mail">
+    <span style="color: red;" class="error">* <?php echo $mailErr;?></span>
+    <br>    
 <label for="link">5.Personal web:<label>
-		<input type="url" id="link" name="link"><br>		
+    <input type="url" id="link" name="link"><br>    
 </fieldset>
 <fieldset>
-	<legend>Account Information: <br></legend>
-	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-	<label for="username">1.UserName:<label>
-		<input type="text" id="username" name="username">
-		<span style="color: red;" class="error">* <?php echo $usernameErr;?></span>
-		<br>
-	<label for="password">2.Password:<label>
-		<input type="password" id="password" name="password">
-		<span style="color: red;" class="error">* <?php echo $passwordErr;?></span>
-		<br>
-	
+  <legend>Account Information: <br></legend>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  <label for="username">1.UserName:<label>
+    <input type="text" id="username" name="username">
+    <span style="color: red;" class="error">* <?php echo $usernameErr;?></span>
+    <br>
+  <label for="password">2.Password:<label>
+    <input type="password" id="password" name="password">
+    <span style="color: red;" class="error">* <?php echo $passwordErr;?></span>
+    <br>
+  
 
 </fieldset>
-		<input type="submit" value="Submit">
-	</form>
+    <input type="submit" value="Submit">
+  </form>
+  </body>
+</html>
 
 <?php
 echo "<h2>Your Input:</h2>";
@@ -154,6 +153,3 @@ echo "<br>";
 echo $password;
 
 ?>
-
-</body>
-</html>
